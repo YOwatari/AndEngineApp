c52726ff8763d3d1d95e8d5178930d62136334e0
YOwatari <yyutao88@gmail.com>
2013-09-21 21:11:06 +0900

    プレイヤーターンまで完成

--- src/com/crossfeel/app/MainGameScene.java
+++ src/com/crossfeel/app/MainGameScene.java
@@ -2,6 +2,7 @@ package com.crossfeel.app;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.microedition.khronos.opengles.GL10;
@@ -30,8 +31,13 @@ public class MainGameScene extends KeyListenScene implements
 	private Text infoText;
 	private Rectangle btn;
 
+	private Text chronoMeterText;
+
+	private List<Text> roleText = new ArrayList<Text>();
+
 	private Rectangle[] boxSprite = new Rectangle[6];
-	private String[] role = { "人狼", "人狼", "村人", "村人", "占い師", "怪盗" };
+	// private String[] role = { "人狼", "人狼", "村人", "村人", "占い師", "怪盗" };
+	private String[] role = { "占い師", "怪盗", "人狼", "人狼", "村人", "村人", };
 
 	// 12色のパステルカラー
 	private String[] colorHex = { "#ff7f7f", "#ff7fbf", "#bf7fff", "#7f7fff",
@@ -44,9 +50,21 @@ public class MainGameScene extends KeyListenScene implements
 	private int phase = PLAYERS_JOIN;
 
 	private int turnPlayer = 0;
+	private boolean goNextPlayer = false;
+
+	private boolean FortuneTellerTurn = false;
+	private boolean JinrohTurn = false;
+	private boolean PhantomThiefTurn = false;
+
+	private int stealPlayerNumber = 7;
+	private int PhantomThiefNumber = 7;
+
+	private int chronoCount = 0;
+	private boolean waitFlag = false;
 
 	private List<Integer> players = new ArrayList<Integer>();
 
+	// コンストラクタ
 	public MainGameScene(MultiSceneActivity context) {
 		super(context);
 		init();
@@ -90,7 +108,7 @@ public class MainGameScene extends KeyListenScene implements
 	 * 箱を配置する
 	 */
 	public void setBoxies() {
-		RoleShuffle(role);
+		// RoleShuffle(role);
 		for (int i = 0; i < boxSprite.length; i++) {
 			float size = getBaseActivity().getEngine().getCamera().getHeight() / 10.0f;
 			float margin = size / 2.0f;
@@ -185,17 +203,115 @@ public class MainGameScene extends KeyListenScene implements
 				/ 2.0f - t.getWidth() / 2.0f, t.getY());
 		return t;
 	}
-	
-	private void handToNext () {
-		TimerHandler playerToCheckMessage = new TimerHandler(3.0f,
+
+	private void SetChronometer(int count) {
+		chronoCount = count * 100;
+		// テキストを書く準備
+		Texture texture = new BitmapTextureAtlas(getBaseActivity()
+				.getTextureManager(), 512, 512,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		Font font = new Font(getBaseActivity().getFontManager(), texture,
+				Typeface.DEFAULT_BOLD, 80, true, Color.WHITE);
+		getBaseActivity().getTextureManager().loadTexture(texture);
+		getBaseActivity().getFontManager().loadFont(font);
+
+		// テキスト表示
+		String secounds = String.format("%02d", chronoCount / 100);
+		String centiSec = String.format("%02d", chronoCount % 100);
+		chronoMeterText = new Text(0, 0, font, secounds + "'" + centiSec, 80,
+				new TextOptions(HorizontalAlign.CENTER), getBaseActivity()
+						.getVertexBufferObjectManager());
+		chronoMeterText.setPosition(getBaseActivity().getEngine().getCamera()
+				.getWidth()
+				/ 2.0f - chronoMeterText.getWidth() / 2.0f, getBaseActivity()
+				.getEngine().getCamera().getHeight()
+				/ 2.0f / 3.0f - chronoMeterText.getHeight() / 2.0f);
+		attachChild(chronoMeterText);
+
+		TimerHandler chronometerHandler = new TimerHandler(1.0f / 60.0f, true,
 				new ITimerCallback() {
 
 					@Override
 					public void onTimePassed(TimerHandler pTimerHandler) {
+						chronoCount--;
+						String secounds = String.format("%02d",
+								chronoCount / 100);
+						String centiSec = String.format("%02d",
+								chronoCount % 100);
+						chronoMeterText.setText(secounds + "'" + centiSec);
+
+						if (chronoCount == 0) {
+							unregisterUpdateHandler(pTimerHandler);
+							detachChild(chronoMeterText);
+							if (!roleText.isEmpty()) {
+								for (Iterator<Text> iterator = roleText
+										.iterator(); iterator.hasNext();) {
+									Text rt = (Text) iterator.next();
+									detachChild(rt);
+								}
+								roleText.clear();
+							}
+
+							infoText = changeText(infoText, "この色のプレイヤーに渡してください");
+							btn.setColor(boxSprite[players.get(turnPlayer)]
+									.getColor());
+							checkNextPlayer();
+						}
+						pTimerHandler.reset();
+
+					}
+				});
+
+		registerUpdateHandler(chronometerHandler);
+	}
+
+	private void handToNext(boolean f) {
+		waitFlag = true;
+		int waitSeconds = 10;
+		SetChronometer(waitSeconds);
+
+		if (f) {
+			infoText = changeText(infoText, "しばらくお待ちください");
+		}
+	}
+
+	private void checkNextPlayer() {
+		TimerHandler playerToCheckMessage = new TimerHandler(3.0f,
+				new ITimerCallback() {
+					@Override
+					public void onTimePassed(TimerHandler pTimerHandler) {
 						infoText = changeText(infoText, "あなたはこの色のプレイヤーですか？");
+						waitFlag = false;
 					}
 				});
 		registerUpdateHandler(playerToCheckMessage);
+	}
+
+	private void setRoleText(Rectangle bs) {
+		BoxInfo bi = (BoxInfo) bs.getUserData();
+
+		// テキストを書く準備
+		Texture texture = new BitmapTextureAtlas(getBaseActivity()
+				.getTextureManager(), 512, 512,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		Font font = new Font(getBaseActivity().getFontManager(), texture,
+				Typeface.DEFAULT_BOLD, 26, true, Color.WHITE);
+		getBaseActivity().getTextureManager().loadTexture(texture);
+		getBaseActivity().getFontManager().loadFont(font);
+
+		// テキスト表示
+		Text roleText = new Text(0, 0, font, bi.role, 30, new TextOptions(
+				HorizontalAlign.CENTER), getBaseActivity()
+				.getVertexBufferObjectManager());
+
+		float x = (bs.getX() + bs.getWidth() / 2.0f) - roleText.getWidth()
+				/ 2.0f;
+		float y = (bs.getY() + bs.getHeight() / 2.0f) - roleText.getHeight()
+				/ 2.0f;
+		roleText.setPosition(x, y);
+
+		attachChild(roleText);
+		this.roleText.add(roleText);
 	}
 
 	// タッチイベントに対する処理
@@ -248,45 +364,125 @@ public class MainGameScene extends KeyListenScene implements
 						phase = PLAYERS_TURN;
 
 						// 次フェーズの初期画面
+						waitFlag = true;
 						infoText = changeText(infoText, "この色のプレイヤーへ渡してください");
 						btn.setColor(boxSprite[players.get(turnPlayer)]
 								.getColor());
 
-						handToNext();
+						TimerHandler playerToCheckMessage = new TimerHandler(
+								2.0f, new ITimerCallback() {
+
+									@Override
+									public void onTimePassed(
+											TimerHandler pTimerHandler) {
+										infoText = changeText(infoText,
+												"あなたはこの色のプレイヤーですか？");
+										btn.setColor(boxSprite[players
+												.get(turnPlayer)].getColor());
+										waitFlag = false;
+									}
+								});
+						registerUpdateHandler(playerToCheckMessage);
 					}
 				}
 				break;
 			case PLAYERS_TURN:
 				if (bi != null) {
 					if (bi.turn && !bi.playerChecked) {
-						infoText = changeText(infoText, bi.role);
+						infoText = changeText(infoText, "あなたは「" + bi.role
+								+ "」です");
 						bi.playerChecked = true;
+					}
+					if (FortuneTellerTurn && bi.role != "占い師") {
+						if (bi.player) {
+							setRoleText(boxSprite[bi.number]);
+						} else {
+							for (int i = 0; i < boxSprite.length; i++) {
+								BoxInfo tmpbi = (BoxInfo) boxSprite[i]
+										.getUserData();
+								if (!tmpbi.player) {
+									setRoleText(boxSprite[tmpbi.number]);
+								}
+							}
+						}
+						FortuneTellerTurn = false;
+						infoText = changeText(infoText, "しばらくお待ちください");
+					}
+					if (PhantomThiefTurn && bi.role != "怪盗" && bi.player) {
+						setRoleText(boxSprite[bi.number]);
+						stealPlayerNumber = bi.number;
+						PhantomThiefTurn = false;
+						infoText = changeText(infoText, "しばらくお待ちください");
 					}
 				} else {
 					if (touchedInfo) {
-						if (turnPlayer < players.size()) {
-							bi = (BoxInfo) boxSprite[players.get(turnPlayer)]
-									.getUserData();
-							if (bi.playerChecked) {
-								turnPlayer++;
-								if (turnPlayer != players.size()){
-									infoText = changeText(infoText,
-											"この色のプレイヤーへ渡してください");
-									btn.setColor(boxSprite[players.get(turnPlayer)]
-											.getColor());
+						if (!waitFlag) {
+							if (turnPlayer < players.size()) {
+								bi = (BoxInfo) boxSprite[players
+										.get(turnPlayer)].getUserData();
+								if (bi.playerChecked) {
+									if (bi.role == "占い師" && !FortuneTellerTurn) {
+										infoText = changeText(infoText,
+												"占い先を選んでください");
+										FortuneTellerTurn = true;
+									} else if (bi.role == "人狼") {
+										infoText = changeText(infoText,
+												"仲間の人狼を確認してください");
+										for (Iterator<Integer> iterator = players
+												.iterator(); iterator.hasNext();) {
+											Integer p = (Integer) iterator
+													.next();
+											BoxInfo tmpbi = (BoxInfo) boxSprite[p]
+													.getUserData();
+											if (tmpbi.role == "人狼") {
+												setRoleText(boxSprite[p]);
+											}
+										}
+										JinrohTurn = true;
+									} else if (bi.role == "怪盗") {
+										PhantomThiefNumber = bi.number;
+										infoText = changeText(infoText,
+												"怪盗先を選んでください");
+										PhantomThiefTurn = true;
+									} else {
+										goNextPlayer = true;
+									}
 
-									handToNext();
-								}
-							} else {
-								if (!bi.turn) {
-									infoText = changeText(infoText,
-											"あなたの役割を確認してください");
-									bi.turn = true;
+									// 次のユーザへ渡す
+									if (goNextPlayer || FortuneTellerTurn
+											|| JinrohTurn || PhantomThiefTurn) {
+										turnPlayer++;
+										if (turnPlayer != players.size()) {
+											handToNext(goNextPlayer);
+										} else {
+											if (stealPlayerNumber < boxSprite.length
+													&& PhantomThiefNumber < boxSprite.length) {
+												BoxInfo ptbi = (BoxInfo) boxSprite[PhantomThiefNumber]
+														.getUserData();
+												BoxInfo stealbi = (BoxInfo) boxSprite[stealPlayerNumber]
+														.getUserData();
+												ptbi.role = stealbi.role;
+												stealbi.role = "怪盗";
+											}
+											phase = TALKING;
+											infoText = changeText(infoText,
+													"議論スタート");
+											
+											// デバッグ用
+											for (int i = 0; i < boxSprite.length; i++) {
+												setRoleText(boxSprite[i]);
+											}
+										}
+									}
+
+								} else {
+									if (!bi.turn) {
+										infoText = changeText(infoText,
+												"あなたの役割を確認してください");
+										bi.turn = true;
+									}
 								}
 							}
-						} else {
-							phase = TALKING;
-							infoText = changeText(infoText, "議論スタート");
 						}
 					}
 				}
